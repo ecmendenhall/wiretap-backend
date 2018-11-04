31c83d2becbc8870bfa805f3646b9e2f46c9d485
Connor Mendenhall <ecmendenhall@gmail.com>
2018-11-04 15:29:10 -0500

    Add Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :wiretap, WiretapWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "wiretap-backend.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :wiretap, Wiretap.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,7 +66,3 @@ config :logger, level: :info
 #
 #     config :wiretap, WiretapWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
